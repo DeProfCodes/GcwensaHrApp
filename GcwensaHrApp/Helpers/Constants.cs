cf3dbc42bfb43761019d14837cd9d402eb72af53
Proficient Mkansi <de.prof.codes@gmail.com>
2023-01-14 09:42:19 +0200

    - add header and footer - style login page - add responsiveness

---
+++ GcwensaHrApp/Helpers/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace GcwensaHrApp.Helpers
+{
+    public class Constants
+    {
+        static class Theme
+        {
+            public static string BrandColor = "#ff8400";
+        }
+    }
+}
